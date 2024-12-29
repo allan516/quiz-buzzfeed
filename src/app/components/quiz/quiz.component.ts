4e1b19d237cf30fb456b853b2f3eefde38c88e05
Allan <allan.mendes0976@gmail.com>
2024-12-29 11:54:03 -0300

    feat: add quiz.component

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.css']
+  styleUrls: ['./quiz.component.css'],
 })
 export class QuizComponent implements OnInit {
+  title: string = '';
 
-  constructor() { }
+  questions: any;
+  questionSelected: any;
 
-  ngOnInit(): void {
-  }
+  answers: string[] = [];
+  answerSelected: string = '';
 
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+  constructor() {}
+
+  ngOnInit(): void {}
 }
